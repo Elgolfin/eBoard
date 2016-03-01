4f9ceddeee0be956189ba48be4876a0aada623bc
Elgolfin <elgo@nicomputer.net>
2016-02-29 21:21:35 -0500

    Put the line chart data into the external KPIs JSON file

--- components/charts/kpi.vue.js
+++ components/charts/kpi.vue.js
@@ -36,51 +36,7 @@ exports.kpi = Vue.extend({
         },
         
         lineData: function () {
-            return {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [
-                    {
-                        label: "Baseline",
-                        fillColor: "rgba(220,220,220,0.1)",
-                        strokeColor: "grey",
-                        pointColor: "grey",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [0, 0, 0, 0, 0, 0, 0]
-                    },
-                    {
-                        label: "My First dataset",
-                        fillColor: "rgba(220,220,220,0.1)",
-                        strokeColor: "green",
-                        pointColor: "green",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [90, 90, 90, 90, 90, 90, 90]
-                    },
-                    {
-                        label: "My First dataset",
-                        fillColor: "rgba(220,220,220,0.1)",
-                        strokeColor: "orange",
-                        pointColor: "orange",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [80, 80, 80, 80, 80, 80, 80]
-                    },
-                    {
-                        label: "My Second dataset",
-                        fillColor: "rgba(220,220,220,0.2)",
-                        strokeColor: "coral",
-                        pointColor: "coral",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [28, 48, 40, 19, 86, 27, 90]
-                    }
-                ]
-            }
+             return this.kpidata.lineData;
         }
         // */
     },
--- components/charts/kpis.vue.js
+++ components/charts/kpis.vue.js
@@ -6,35 +6,7 @@ var Vue = require('vue');
 exports.kpis = Vue.extend({
     data: function () {
         return {
-            // TODO: dynamically load the kpis from a json file
             kpis: jetpack.read('./components/charts/kpis.json','json')
-            /*kpis: [
-                {
-                    greenTarget: 80,
-                    yellowTarget: 60,
-                    actual: 75,
-                    title: "My KPI 1",
-                    id: "my-kpi1",
-                    lineData: null
-                },
-                {
-                    greenTarget: 90,
-                    yellowTarget: 80,
-                    actual: 79,
-                    title: "My KPI 2",
-                    id: "my-kpi2",
-                    lineData: null
-                },
-                {
-                    greenTarget: 95,
-                    yellowTarget: 90,
-                    actual: 50,
-                    title: "My KPI 3",
-                    id: "my-kpi3",
-                    lineData: null
-                }
-            ]    
-            //*/
         }  
     },
     template: jetpack.read('./components/charts/kpis.vue.html'),
